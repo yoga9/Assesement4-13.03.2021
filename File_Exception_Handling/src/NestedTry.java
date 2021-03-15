9ccbabb3f457ae17541c05153ef3a7a9dfc6f7dd
YogV2C18463 <YogV2C18463@V2CP0068.vee2office.com>
2021-03-15 17:23:10 +0530

    First commit

---
+++ File_Exception_Handling/src/NestedTry.java
@@ -0,0 +1,35 @@
+
+public class NestedTry {
+	     
+		public static void main(String[] args) {
+			
+			try {
+				
+				try {
+				int a = 5/0;
+			}
+				catch(ArrayIndexOutOfBoundsException e) {
+					System.out.println(e);
+				}
+				
+				try {
+					int a[] = new int[5];
+				    a[6] = 10;
+				}
+			   
+				catch(ArithmeticException e) {
+					System.out.println(e);
+				}
+			}
+			
+				
+			catch(Exception e) {
+				System.out.println(e);
+				e.getStackTrace();
+			}
+				
+			
+		}}
+
+
+
